3e158a282277dfea6188e60adc097caf4da5b828
Nemo <XZC168520@163.com>
2016-11-17 15:16:19 +0800

    Initial Commit

---
+++ ScreenshotsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScreenshotsDemo
+//
+//  Created by Mjwon on 2016/11/17.
+//  Copyright © 2016年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
